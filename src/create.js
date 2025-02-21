2648b0e0f05cf148bf4e2f7867cb1a56ac5f4b3a
Willyams Yujra <yracnet@gmail.com>
2025-02-20 21:18:03 -0400

    + Update Page

--- src/create.js
+++ src/create.js
@@ -4,6 +4,46 @@ import path from "path";
 
 const fileData = path.resolve("src/data.json");
 const filePage = path.resolve("public/index.md");
+const fileXml = path.resolve("public/sitemap.xml");
+
+const token = "";
+
+const doSitemap = (list) => {
+  let sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">`;
+
+  list.forEach((repo) => {
+    sitemap += `
+  <url>
+    <loc>${repo.page_url}</loc>
+    <lastmod>${new Date().toISOString()}</lastmod>
+    <changefreq>monthly</changefreq>
+    <priority>0.8</priority>
+  </url>`;
+  });
+
+  sitemap += `
+</urlset>`;
+  return sitemap;
+};
+
+const doIndex = (list) => {
+  const html = list
+    .map((it) => {
+      return `
+## ${it.name}
+${it.description || ""}
+  - [${it.html_url}](${it.html_url})
+  - [${it.page_url}](${it.page_url})
+`.trim();
+    })
+    .join("\n\n");
+  return `
+# Repositories
+
+${html}
+  `;
+};
 
 const run = async () => {
   const exist = fs.existsSync(fileData);
@@ -12,10 +52,11 @@ const run = async () => {
       "https://api.github.com/users/yracnet/repos?per_page=200",
       {
         headers: {
-          Authorization: "Bearer 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",
+          Authorization: `Bearer ${token}`,
         },
       }
     );
+    console.log(resp);
     const data = await resp.json();
     const list = data
       .filter((it) => !it.private)
@@ -35,26 +76,12 @@ const run = async () => {
   }
   const data = fs.readFileSync(fileData);
   const list = JSON.parse(data);
-  const html = list
-    .map((it) => {
-      return `
-## ${it.name}
-${it.description || ""}
-  - [${it.html_url}](${it.html_url})
-  - [${it.page_url}](${it.page_url})
-`;
-    })
-    .join("\n\n");
+  const html = doIndex(list);
   console.log("Read: ", fileData);
   console.log("Write: ", filePage);
-  fs.writeFileSync(
-    filePage,
-    `
-# Repositories
-
-${html}  
-  
-  `
-  );
+  fs.writeFileSync(filePage, html);
+  const xml = doSitemap(list);
+  console.log("Write: ", fileXml);
+  fs.writeFileSync(fileXml, xml);
 };
 run();
